ca43cbbbe27211179362fc0aedc342e89596600d
Kevin Kuszyk <kevin@kevinkuszyk.com>
2014-09-26 10:11:37 +0100

    Initial commit

---
+++ src/rs/ReportService2010/ReportingServiceChannelFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace Rs.ReportService2010
+{
+    public class ReportingServiceChannelFactory
+    {
+        public IReportingService2010 Create(string url)
+        {
+            var endpoint = new EndpointAddress(url);
+            var binding = endpoint.Uri.Scheme == "https" ? new BasicHttpsBinding() as Binding : new BasicHttpBinding();
+            var factory = new ChannelFactory<IReportingService2010>(binding);
+
+            return factory.CreateChannel(endpoint);
+        }
+    }
+}
