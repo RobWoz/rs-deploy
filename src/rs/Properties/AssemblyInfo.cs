e09bc6f92a81ef87b4a971f198d8c0dd41182cd8
Kevin Kuszyk <kevin@kevinkuszyk.com>
2014-09-30 10:11:13 +0100

    Bump version to 1.0.2

--- src/rs/Properties/AssemblyInfo.cs
+++ src/rs/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using CommandLine;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.2")]
 
 [assembly: AssemblyUsage(
     @"Usage: rs upload-file -f 'C:\folder.sub-folder.report.rdl' -d '/test' -s server",
