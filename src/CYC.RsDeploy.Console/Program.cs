dca31a4e1203ed9a47a0430565e4609400ab00f5
Kevin Kuszyk <kevin@kevinkuszyk.com>
2015-12-14 12:48:17 +0000

    Update nlog to latest version

--- src/CYC.RsDeploy.Console/Program.cs
+++ src/CYC.RsDeploy.Console/Program.cs
@@ -39,7 +39,7 @@ namespace CYC.RsDeploy.Console
             }
             catch (Exception ex)
             {
-                logger.Error("Unexpected error", ex);
+                logger.Error(ex);
                 Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
             }
             
